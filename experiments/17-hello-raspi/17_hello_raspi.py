110dc29ef7e572762a1b48d342262e5816a9418b
paulabuchelif <mariabucheli@unicauca.edu.co>
2019-07-19 18:48:36 -0500

    add time sleep into raspi hello file

--- experiments/17-hello-raspi/17_hello_raspi.py
+++ experiments/17-hello-raspi/17_hello_raspi.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as gpio
+import time 
 #para iniciar la board:
 gpio.setmode(gpio.BOARD) 
 gpio.setup(12,gpio.OUT)  #pin 12 es de salida
 gpio.output(12, True)
+time.sleep(20)
 gpio.output(12, False)
+gpio.cleanup(12)
