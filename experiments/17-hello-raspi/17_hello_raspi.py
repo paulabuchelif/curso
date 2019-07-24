5e5015b2113264c920a315bb1b0dbdd8d6bc6abb
paulabuchelif <mariabucheli@unicauca.edu.co>
2019-07-24 17:11:29 -0500

    add raspi sample

--- experiments/17-hello-raspi/17_hello_raspi.py
+++ experiments/17-hello-raspi/17_hello_raspi.py
@@ -3,14 +3,17 @@ import time
 #para iniciar la board:
 gpio.setmode(gpio.BOARD) 
 gpio.setup(24,gpio.OUT)  #pin 12 es de salida
-gpio.setup(23, gpio.IN)
+gpio.setup(23, gpio.IN, pull_up_down = gpio.PUD_UP)
 # gpio.output(24, True)
 # time.sleep(20)
 # gpio.output(24, False)
 # gpio.cleanup(24)
 while True:
     input_state = GPIO.input(23)
-    if input_state = False:
+    if input_state == False:
         gpio.gpio.output(24, True)
     else:
     gpio.gpio.output(24, False) 
+time.sleep(0.3)
+gpio.cleanup(23)
+gpio.cleanup(24)
